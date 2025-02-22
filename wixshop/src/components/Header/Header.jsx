49fd95a55c83328a7373ce2a3b1a6687a207ace7
aliviee <legoromanov67@gmail.com>
2025-02-22 16:43:36 +0200

    startt

---
+++ wixshop/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import s from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+      <p className={s.sdfsdf}>lksjdjgfjksgdfjkg</p>
+        <div class={s.menu}>
+            <input type="checkbox" id="burgerCheckbox" class={s.burgerCheckbox}/>
+            <label for="burgerCheckbox" class={s.burger}></label>
+            <ul class={s.menuList}>
+                <li><a href="#" class={s.menuItem}>Главная</a></li>
+                <li><a href="#" class={s.menuItem}>О нас</a></li>
+                <li><a href="#" class={s.menuItem}>Команда</a></li>
+                <li><a href="#" class={s.menuItem}>Контакты</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
