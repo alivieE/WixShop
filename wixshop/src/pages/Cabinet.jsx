49bf54fe566478e538cb4dde3e09d4e7b04e46da
aliviee <legoromanov67@gmail.com>
2025-03-08 15:33:52 +0200

    add styles for main page

--- wixshop/src/pages/Cabinet.jsx
+++ wixshop/src/pages/Cabinet.jsx
@@ -10,27 +10,30 @@ const Cabinet = () => {
       <Header></Header>
       <Hero></Hero>
       <section>
-        <p>Enjoy Our Special Recommendations</p>
-        <ul>
+        <p className={s.elementStyle}>Enjoy Our Special Recommendations</p>
+        <ul className={s.uul}>
           <li className={s.lii}>
             <img src={images.cheess} />
             <p>FRENCH BRIE CHEESE</p>
-            <div></div>
+            <div>&mdash;</div>
             <p>$6.99</p>
           </li>
           <li className={s.lii}>
             <img src={images.bacon} />
             <p>smoked virgina bacon</p>
-            <div></div>
+            <div>&mdash;</div>
             <p>$12.99</p>
           </li>
           <li className={s.lii}>
             <img src={images.oliva} />
             <p>GREEK KALAMATA OLIVES</p>
-            <div></div>
+            <div>&mdash;</div>
             <p>$8.99</p>
           </li>
         </ul>
+        <div className={s.bottomText}>
+          <p className={s.bottomTextWrap}>© 2035 by GOOD TO EAT. Powered and secured by </p>
+        </div>
       </section>
     </div>
   );
