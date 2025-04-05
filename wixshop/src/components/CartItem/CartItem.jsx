9d5720087264048962d89919778d58899e82e46b
aliviee <legoromanov67@gmail.com>
2025-04-05 14:53:13 +0300

    add little part of CartItem

--- wixshop/src/components/CartItem/CartItem.jsx
+++ wixshop/src/components/CartItem/CartItem.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import s from "./CartItem.module.css";
 
-const CartItem = () => {
+const CartItem = ({ product, productList }) => {
   return (
     <div>
-      
+      <p>{product.id}</p>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
