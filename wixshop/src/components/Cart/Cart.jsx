4b7d96e7eed46197a14cd632e4b25fc80a5cc68a
aliviee <legoromanov67@gmail.com>
2025-04-05 12:56:16 +0300

    add styles for Cart

--- wixshop/src/components/Cart/Cart.jsx
+++ wixshop/src/components/Cart/Cart.jsx
@@ -26,17 +26,56 @@ const Cart = ({ openCart, setOpenCart, productList }) => {
           ></path>
         </svg>
       </div>
-      <h2 className={s.text}>Cart </h2>
-      <div className={s.horizontalLine}></div>
-      <ul>
-        {productList.map((product) => {
-          return (
-            <li>
-              <CartItem product={product} />
-            </li>
-          );
-        })}
-      </ul>
+      <div className={s.listWrap}>
+        <h2 className={s.text}>Cart </h2>
+        <div className={s.horizontalLine}></div>
+        <ul className={s.list}>
+          {productList.map((product) => {
+            return (
+              <li>
+                <CartItem product={product} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className={s.subWrap}>
+        <div className={s.subPrice}>
+          <h2 className={s.subtotal}>Subtotal</h2>
+          <h2 className={s.price}>$74.92</h2>
+        </div>
+        <div className={s.description}>
+          <p>Taxes and shopping are caulated at checkout</p>
+        </div>
+        <button type="button" className={s.btnCheck}>
+          Checkout
+        </button>
+
+        <button type="button" className={s.btnViewCart}>
+          View Cart
+        </button>
+        <div className={s.lock}>
+          <p className={s.lock}>
+            <svg
+              width="11"
+              height="14"
+              viewBox="0 0 11 14"
+              xmlns="http://www.w3.org/2000/svg"
+              class="QXycij"
+              data-hook="SecureCheckoutDataHook.lock"
+            >
+              <g fill="currentColor" fill-rule="evenodd">
+                <path d="M0 12.79c0 .558.445 1.01.996 1.01h9.008A1 1 0 0 0 11 12.79V6.01c0-.558-.445-1.01-.996-1.01H.996A1 1 0 0 0 0 6.01v6.78Z"></path>
+                <path
+                  d="M9.5 5v-.924C9.5 2.086 7.696.5 5.5.5c-2.196 0-4 1.586-4 3.576V5h1v-.924c0-1.407 1.33-2.576 3-2.576s3 1.17 3 2.576V5h1Z"
+                  fill-rule="nonzero"
+                ></path>
+              </g>
+            </svg>
+            Secure Checkout
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
